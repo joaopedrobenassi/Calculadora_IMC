e310981584d38b6e61b1ee72b228c1f85c577835
joaopedrobenassi <joaopedrobenassi@gmail.com>
2022-11-29 16:17:33 -0300

    Determinar ids / Criação de variaveis e eventos

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
 
-function imc(){
+const calcular = document.getElementById('calcular') //Selecionando o botão calcular
 
+function imc(){ // inicializando função
+    const nome = document.getElementById('nome')
+    const altura = document.getElementById('altura')
+    const peso = document.getElementById('peso')
+    const resultado = document.getElementById('resultado')
 }
+
+calcular.addEventListener('click', imc) //Evento de click
