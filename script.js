93b949c09fa49e5007e0b47f8c8ea19dd73b5913
joaopedrobenassi <joaopedrobenassi@gmail.com>
2022-11-30 18:48:45 -0300

    Atualizando for Alteracao de estilo continuacao de funcao

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ function imc(){ // inicializando função
     const altura = document.getElementById('altura')
     const peso = document.getElementById('peso')
     const resultado = document.getElementById('resultado')
+
+    if (nome.value !== '' && altura.value !== '' && peso.value !== ''){
+
+        const valorImc = (peso.value /(altura.value * altura.value)).toFixed(1)
+
+        let classificacao = ''
+
+        if(valorImc < 18.5){
+
+            classificacao = 'Abaixo do peso'
+        }
+
+        resultado.textContent = `${nome.value} seu IMC é ${valorImc} e você está ${classificacao}`
+    }
 }
 
 calcular.addEventListener('click', imc) //Evento de click
